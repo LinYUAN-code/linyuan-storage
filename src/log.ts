ecd38d827034cfab624c9946ed49275a2fd540df
renjunlin <renjunlin@tencent.com>
2022-11-24 20:20:51 +0800

    fix: 修复ts error

--- src/log.ts
+++ src/log.ts
@@ -1,40 +1,36 @@
-
-
-enum LOG_LEVEL{
-    Normal = 0,
-    Error,
+enum LOG_LEVEL {
+  Normal = 0,
+  Error,
 }
 let __log_level = LOG_LEVEL.Normal;
 
 function setLogLevel(logLevel: LOG_LEVEL) {
-    console.log("setLogLevel:", logLevel);
-    __log_level = logLevel;
+  console.log("setLogLevel:", logLevel);
+  __log_level = logLevel;
 }
 
-
-function normal(...msg) {
-    if(__log_level <= LOG_LEVEL.Normal) {
-        console.log(...msg);
-    }
+function normal(...msg: any[]) {
+  if (__log_level <= LOG_LEVEL.Normal) {
+    console.log(...msg);
+  }
 }
 
-function error(...msg) {
-    if(__log_level <= LOG_LEVEL.Error) {
-        console.log(...msg);
-    }
+function error(...msg: any[]) {
+  if (__log_level <= LOG_LEVEL.Error) {
+    console.log(...msg);
+  }
 }
 
 type l_log = {
-    setLogLevel: (LOG_LEVEL) => void,
-    normal: (...any) => void,
-    error: (...any) => void,
-}
+  setLogLevel: (arg0: LOG_LEVEL) => void;
+  normal: (...arg0: any[]) => void;
+  error: (...arg0: any[]) => void;
+};
 
 let log: l_log = {
-    normal,
-    error,
-    setLogLevel
+  normal,
+  error,
+  setLogLevel,
 };
 
 export default log;
-
